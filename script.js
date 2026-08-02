c168237375cf0b5a923a0ff94fefb18f7a4c42de
Désiré Compaoré <ton-email@example.com>
2026-08-02 20:58:01 +0000

    Amélioration pages secondaires et commandes WhatsApp

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Désiré Digital Service chargé avec succès");
 
 });
+
+
 function commander(service) {
 
     const numero = "22669385149";
 
-    const message = "Bonjour Désiré Digital Service, je souhaite commander : " + service;
+    const message =
+        "Bonjour Désiré Digital Service 👋\n\n" +
+        "Je souhaite acheter : " + service + "\n\n" +
+        "Merci de me donner les informations pour la commande.";
 
-    const url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(message);
+    const url =
+        "https://wa.me/" + numero +
+        "?text=" + encodeURIComponent(message);
 
     window.open(url, "_blank");
 
